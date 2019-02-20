a6ff011a569d5b35ccf0213a99a9bb7835ae668c
shailesh <shailesh.kumarcs@gmail.com>
2019-02-20 22:27:51 +0530

    Interceptor about

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+
   render() {
     return (
       <div>
